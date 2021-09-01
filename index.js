ff0e0f250e462309e94942eeb1bc4f5d11d3bef6
sdefreese <samanthadefreese1@gmail.com>
2021-09-01 15:34:47 -0700

    commit tree a9e2631c61a184823e04a6dfe19e39f79f7ad0ee

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function receivesAFunction (otherFunction) {
+    otherFunction();
+    }
+function returnsANamedFunction() {
+    return function otherFunctionTwo() {
+    }
+    }
+function returnsANamedFunction() {
+    return function otherFunctionTwo(){
+     return 2+2
+    }
+}
+function returnsAnAnonymousFunction() {
+    return function () {
+        return "something"
+    }
+}
